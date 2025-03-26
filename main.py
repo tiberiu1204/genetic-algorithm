d29459a1ed106ba1da9cc6c2b40a26fc420f1820
Popescu Tiberiu <tiberiu1204@gmail.com>
2025-03-26 18:39:53 +0200

    Implemented crossover function

--- main.py
+++ main.py
@@ -57,7 +57,8 @@ def population_to_values(params, population):
     return ((b - a) / ((1 << chromosome_len) - 1)) * population + a
 
 
-def terminate(t):
+def terminate(params, t):
+    # return params["num_generations"] == t
     return t == 1
 
 
@@ -103,11 +104,75 @@ def selection(params, population, out_file=None):
     return population[selections], elitist
 
 
-def crossover(params, population):
-    pass
+def crossover(params, population, out_file=None):
+    def print_rolls():
+        index = 0
+        for individual in old_population:
+            out_file.write(
+                f"{index + 1}: {individual:0{chromosome_len}b} u={u[index]}")
+            if u[index] < probability:
+                out_file.write(f"<{probability} participa\n")
+            else:
+                out_file.write("\n")
+            index += 1
+
+    def print_crossovers():
+        index = 0
+        out_file.write("\n")
+        while index < len(points):
+            chr1_index = selected_indexes[index * 2]
+            chr2_index = selected_indexes[index * 2 + 1]
+            chr1 = old_population[chr1_index]
+            chr2 = old_population[chr2_index]
+
+            point = points[index]
+            out_file.write(f"Recombinare dintre cromozomul {
+                           chr1_index + 1} cu cromozomul {chr2_index + 1}:\n")
+            out_file.write(f"{chr1:0{chromosome_len}b} {
+                           chr2:0{chromosome_len}b} punct {point}\n")
+            out_file.write(f"Rezultat {population[chr1_index]:0{chromosome_len}b} {
+                           population[chr2_index]:0{chromosome_len}b}\n")
+            index += 1
+
+    old_population = np.copy(population)
+
+    probability, chromosome_len = params["crossover_probability"], params["chromosome_len"]
+    u = np.random.uniform(size=len(population))
+    mask = u < probability
+    selected = population[mask]
+    temp = None
+    if len(selected) % 2 == 1:
+        temp = selected[-1]
+        selected = selected[:-1]
+    selected_indexes = np.where(mask)[0]
+    sample1 = selected[::2]
+    sample2 = selected[1::2]
+    points = np.random.randint(low=0, high=chromosome_len, size=len(sample1))
+
+    tail_lengths = chromosome_len - points
+    heads1 = sample1 >> tail_lengths
+    heads2 = sample2 >> tail_lengths
+    tails1 = sample1 & ((1 << tail_lengths) - 1)
+    tails2 = sample2 & ((1 << tail_lengths) - 1)
+
+    sample1 = (heads2 << tail_lengths) + tails1
+    sample2 = (heads1 << tail_lengths) + tails2
+
+    selected[::2] = sample1
+    selected[1::2] = sample2
+    if temp:
+        selected = np.append(selected, temp)
+    population[mask] = selected
+
+    if out_file:
+        out_file.write(f"Probabilitate de incrucisare {probability}\n")
+        print_rolls()
+        out_file.write("\n")
+        print_crossovers()
+        print_population(params, population, out_file, "Dupa recombinare")
 
 
-def mutation(params, population):
+def mutation(params, population, out_file=None):
     pass
 
 
@@ -124,12 +189,12 @@ def simulate():
     population = init_population(params)
     print_population(params, population, out_file, "Populatia initiala")
 
-    while not terminate(t):
+    while not terminate(params, t):
+        out_file_first_iteration = None if t else out_file
         population, elitist = selection(
-            params, population, out_file=None if t else out_file)
-        print(population)
-        population = crossover(params, population)
-        population = mutation(params, population)
+            params, population, out_file_first_iteration)
+        population = crossover(params, population, out_file_first_iteration)
+        population = mutation(params, population, out_file_first_iteration)
         population = np.append(population, elitist)
         t += 1
 
